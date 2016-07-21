f0ee60ea7c50e266854fb474add19c3a401d2dbb
iThinkerYZ <690423479@qq.com>
2016-07-22 01:36:17 +0800

    上传 cocoapod.podspec

--- YZDisplayViewControllerDemo/YZDisplayViewControllerDemo/AppDelegate.m
+++ YZDisplayViewControllerDemo/YZDisplayViewControllerDemo/AppDelegate.m
@@ -16,6 +16,8 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    // 
     // Override point for customization after application launch.
     return YES;
 }
